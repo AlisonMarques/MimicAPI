49ae480dc832d13bdcf8bfe69869d0bd3adf4195
AlisonMarques <alison-marques@outlook.com.br>
2021-04-05 09:12:55 -0300

    Desenvolvido a conexao com o banco de dados e a criação do MimicContext

--- MimicAPI/Startup.cs
+++ MimicAPI/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MimicAPI.Database;
 
 namespace MimicAPI
 {
@@ -16,7 +19,12 @@ namespace MimicAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddDbContext<MimicContext>(opt =>
+            {
+                opt.UseSqlite("Data Source=Database//Mimic.db");
+            });
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,7 +36,7 @@ namespace MimicAPI
             }
 
             app.UseMvc();
-            
+
         }
     }
 }
