f23d9a9477f7931f5fba7f8800dc910bebe795c7
AlisonMarques <alison-marques@outlook.com.br>
2021-04-06 14:15:19 -0300

    Criado o primeiro link no método ObterUmaPalavra

--- MimicAPI/Helpers/DTOMapperProfile.cs
+++ MimicAPI/Helpers/DTOMapperProfile.cs
@@ -1,9 +1,21 @@
 using AutoMapper;
+using MimicAPI.Models;
+using MimicAPI.Models.DTO;
 
 namespace MimicAPI.Helpers
 {
     public class DTOMapperProfile : Profile
     {
-        
+        //Config profile
+        public DTOMapperProfile()
+        {
+            // convertendo Palavra para PalavaDTo
+            CreateMap<Palavra, PalavraDto>();
+        }
     }
 }
+
+        /*
+         * AutoMapper é uma biblioteca que vai nos ajudar
+         * a copiar um objeto de um tipo para outro objeto
+         */
