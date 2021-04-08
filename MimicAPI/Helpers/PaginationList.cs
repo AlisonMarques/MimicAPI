0246bc302c23c180cd884cecf79eda6004897444
AlisonMarques <alison-marques@outlook.com.br>
2021-04-08 11:39:49 -0300

    Logica d paginacao next prev e refatoracao codigo

--- MimicAPI/Helpers/PaginationList.cs
+++ MimicAPI/Helpers/PaginationList.cs
@@ -1,9 +1,14 @@
 using System.Collections.Generic;
+using MimicAPI.Models.DTO;
 
 namespace MimicAPI.Helpers
 {
-    public class PaginationList<T> : List<T>
+    public class PaginationList<T>
     {
+        public List<T> Results { get; set; } = new List<T>();
         public Pagination Pagination { get; set; }
+
+        // será os links de paginação para ir para próxima page ou voltar
+        public List<LinkDTO> Links { get; set; } = new List<LinkDTO>();
     }
 }
