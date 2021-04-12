dc29d9b711e45c8f51c09d7fba4344e0cd7f0b8a
AlisonMarques <alison-marques@outlook.com.br>
2021-04-12 11:29:36 -0300

    Configuracao para usar a documentacao do swagger

--- MimicAPI/Startup.cs
+++ MimicAPI/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -14,8 +16,13 @@ using MimicAPI.V1.Repositories;
 using MimicAPI.V1.Repositories.Contracts;
 using MimicAPI.V1.Repositories.Contracts.Contracts;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.OpenApi.Models;
 using MimicAPI.Helpers;
+using MimicAPI.Helpers.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace MimicAPI
 {
@@ -41,6 +48,7 @@ namespace MimicAPI
             
             services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddScoped<IPalavraRepository, PalavraRepository>();
+            
             //controle de versionamento
             services.AddApiVersioning(cfg =>
             {
@@ -53,6 +61,54 @@ namespace MimicAPI
                 cfg.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
             });
 
+            // Configurando o serviço de documentação do Swagger com as versões
+            services.AddSwaggerGen(cfg =>
+            {
+                //resolvendo o problema de conflitos com as rotas
+                cfg.ResolveConflictingActions(apiDescription => apiDescription.First());
+               
+                // definindo no swagger quantas versões a API possui
+                cfg.SwaggerDoc("v2.0", new OpenApiInfo()
+                {
+                    Title = "MimicAPI - V2.0",
+                    Version = "V2.0"
+                });
+                
+                cfg.SwaggerDoc("v1.1", new OpenApiInfo()
+                {
+                    Title = "MimicAPI - V1.1",
+                    Version = "V1.1"
+                });
+                
+                cfg.SwaggerDoc("v1.0", new OpenApiInfo()
+                {
+                    Title = "MimicAPI - V1.0",
+                    Version = "V1.0"
+                });
+                
+                // Configuração para subir os comentários do controller para o Swagger
+                var CaminhoProjeto = PlatformServices.Default.Application.ApplicationBasePath;
+                var NomeProjeto = $"{PlatformServices.Default.Application.ApplicationName}.xml";
+                var CaminhoArquivoXMLComentario = Path.Combine(CaminhoProjeto, NomeProjeto);
+                cfg.IncludeXmlComments(CaminhoArquivoXMLComentario);
+
+                cfg.DocInclusionPredicate((docName, apiDesc) =>
+                {
+                    if (!apiDesc.TryGetMethodInfo(out MethodInfo methodInfo)) return false;
+
+                    var versions = methodInfo.DeclaringType
+                        .GetCustomAttributes(true)
+                        .OfType<ApiVersionAttribute>()
+                        .SelectMany(attr => attr.Versions);
+
+                    return versions.Any(v => $"v{v.ToString()}" == docName);
+
+                });
+                services.AddMvc(cfg => cfg.Conventions.Add
+                    (new ApiExplorerGroupPerVersionConvention()));
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -64,7 +120,22 @@ namespace MimicAPI
             }
 
             app.UseStatusCodePages();
+            
             app.UseMvc();
+
+            // aqui vai criar o arquivo do swagger : /swagger/v1/swagger.json
+            app.UseSwagger();
+            
+            // Ativando middlewares para o uso do Swagger
+            app.UseSwaggerUI(cfg =>
+            {
+                cfg.SwaggerEndpoint("/swagger/v2.0/swagger.json", "MimicAPI - V2.0");
+                cfg.SwaggerEndpoint("/swagger/v1.1/swagger.json", "MimicAPI - V1.1");
+                cfg.SwaggerEndpoint("/swagger/v1.0/swagger.json", "MimicAPI - V1.0");
+                cfg.RoutePrefix = String.Empty;
+            });
+
+
 
         }
     }
